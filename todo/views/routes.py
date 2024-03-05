82e90f889b4591223c47c21a1f9571e458151823
Vy Vu Khanh Ho <116183364+VuKhanhVyHo@users.noreply.github.com>
2024-03-05 23:14:40 +1000

    Add health

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
-
+@api.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+    
 @api.route('/health')
 def health():
     return "ok"
